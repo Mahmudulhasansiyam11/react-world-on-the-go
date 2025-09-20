aaaa753a5d4d1d1c331143803c667926e47d9228
Mahmudulhasansiyam11 <mahmudulsiyam766@gmail.com>
2025-09-19 17:07:39 -0700

    add lift up state and pass handler as props

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
 
     const [visited, setVisited] = useState(false);
 
@@ -16,6 +16,7 @@ const Country = ({country}) => {
         // setVisited(visited ? false : true);
 
         setVisited(!visited);
+        handleVisitedCountries(country);
 
     }
     
